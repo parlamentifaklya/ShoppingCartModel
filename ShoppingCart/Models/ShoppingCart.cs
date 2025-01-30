73597d6908b761833a54ac7b2196d109c9d420cb
parlamentifaklya <20.kokenyesi.andor@mechwart.com>
2025-01-30 08:42:38 +0100

    done

--- ShoppingCart/Models/ShoppingCart.cs
+++ ShoppingCart/Models/ShoppingCart.cs
@@ -12,6 +12,28 @@
         public int ProductCount => _products.Count;
 
         //TODO Készítse el a ShoppingCart osztályban azokat a metódusokat, amelyekkel sikeresen lefutnak a tesztesetek!
+        public void AddProduct (string productName, double price)
+        {
+            Product newProd = new(productName, price);
+            _products.Add(newProd);
+        }
 
+        public bool RemoveProduct(string productName)
+        {
+            var res = _products.Find(prod => prod.Name == productName);
+            return res != null ? _products.Remove(res) : throw new InvalidOperationException("nincs cucc");
+        }
+
+        public double GetTotalPrice()
+        {
+            double totalPrice = 0;
+            _products.ForEach(x => totalPrice += x.Price);
+            return totalPrice;
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _products;
+        }
     }
 }
